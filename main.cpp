d29399de78b3e2b575e21355b31e074a8ad1bfb9
MaSchif <aralas@gmx.net>
2017-08-05 11:57:19 +0200

    Server, Client and Networking (Packages)

--- main.cpp
+++ main.cpp
@@ -1,25 +1,53 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
 #include <cmath>
+#include <set>
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <cstdint>
 
-
-class Player : public sf::RectangleShape {
+class Player : public sf::CircleShape {
   public:
     Player(float width, float height, sf::Color color) :
-      sf::RectangleShape(sf::Vector2f(width, height)),
+      sf::CircleShape(width / 2.f), //(sf::Vector2f(width, height)),
       m_orientation(sf::Vector2f(1, 0)),
       m_velocity(sf::Vector2f(0, 0)),
-      m_position(sf::Vector2f(0, 0))
+      m_position(sf::Vector2f(0, 0)),
+      m_originalColor(color)
     {
         setFillColor(color);
+        setOrigin(sf::Vector2f(width / 2.f, width / 2.f));
         m_speed = 100.;
+        m_isHit = false;
+        m_hitTime = 0;
+    }
+
+    void hit() {
+        m_isHit = true;
+    }
+
+    void update(float delta) {
+        if (m_isHit) {
+            setFillColor(sf::Color::Red);
+            m_hitTime = 0;
+        }
+        if (m_hitTime > 1.) {
+            setFillColor(m_originalColor);
+        }
+        m_hitTime += delta;
+        m_isHit = false;
     }
 
     void move(sf::Vector2f vector) {
-        sf::RectangleShape::move(vector);
+        sf::CircleShape::move(vector);
         m_position += vector;
     }
 
   public:
+    sf::Color m_originalColor;
+    float m_hitTime;
+    bool m_isHit;
     sf::Vector2f m_orientation;
     sf::Vector2f m_velocity;
     sf::Vector2f m_position;
@@ -35,6 +63,7 @@ class Bullet : public sf::CircleShape {
       m_position(position)
     {
         setFillColor(color);
+        setOrigin(sf::Vector2f(radius, radius));
         setPosition(position);
         m_speed = 1000;
     }
@@ -49,8 +78,108 @@ class Bullet : public sf::CircleShape {
     float m_speed;
 };
 
+
+sf::Packet& operator<<(sf::Packet& packet, const Player& player)
+{
+    packet << player.m_position.x << player.m_position.y;
+    packet << player.m_orientation.x << player.m_orientation.y;
+    return packet;
+}
+
+sf::Packet& operator>>(sf::Packet& packet, Player& player)
+{
+    packet >> player.m_position.x >> player.m_position.y;
+    packet >> player.m_orientation.x >> player.m_orientation.y;
+    return packet;
+}
+
+sf::Packet& operator<<(sf::Packet& packet, const Bullet& bullet)
+{
+    packet << bullet.m_position.x << bullet.m_position.y;
+    packet << bullet.m_velocity.x << bullet.m_velocity.y;
+    return packet;
+}
+
+sf::Packet& operator>>(sf::Packet& packet, Bullet& bullet)
+{
+    packet >> bullet.m_position.x >> bullet.m_position.y;
+    packet >> bullet.m_velocity.x >> bullet.m_velocity.y;
+    return packet;
+}
+
+float norm(sf::Vector2f v) {
+    return std::sqrt(v.x * v.x + v.y * v.y);
+}
+
+bool collision(sf::CircleShape circle1, sf::CircleShape circle2)
+{
+    // returns true if they collide
+    return (circle1.getRadius() + circle2.getRadius()) > norm(circle2.getPosition() - circle1.getPosition());
+}
+
+int server(int port)
+{
+    sf::UdpSocket socket;
+    if (socket.bind(port) != sf::Socket::Done) {
+        std::cout << "Server: UdpSocket::bind failed." << std::endl;
+        return 1;
+    }
+
+    std::cout << "Server: Server started." << std::endl;
+    
+    std::set<std::pair<sf::IpAddress, unsigned short>> senderPairList;
+    sf::Packet packet;
+    sf::IpAddress senderAddress;
+    unsigned short senderPort;
+
+    while (true)
+    {
+        if (socket.receive(packet, senderAddress, senderPort) != sf::Socket::Done) {
+            std::cout << "Server: UdpSocket::receive failed." << std::endl;
+            continue;
+        }
+
+        // Packet received
+        auto senderPair = std::make_pair(senderAddress, senderPort);
+        auto isInSet = senderPairList.insert(senderPair);
+
+        if (isInSet.second) {
+            std::cout << "Server: New client:" << senderAddress << ":" << senderPort << std::endl;
+        } else {
+            std::cout << "Server: Packet from known client:" << senderAddress << ":" << senderPort << std::endl;
+        }
+
+        // Send to all clients except sender
+        for (auto receiver : senderPairList) {
+            if (receiver != senderPair) {
+                if (socket.send(packet, receiver.first, receiver.second) != sf::Socket::Done) {
+                    std::cout << "Server: UdpSocket::send failed." << std::endl;
+                }
+            }
+        }
+    }
+    std::cout << "Server: I ripped." << std::endl;
+    return 1;
+}
+
 int main()
 {
+    sf::IpAddress ip("127.0.0.1");
+    unsigned short port = 6768;
+
+    // Start local server
+    std::thread serverThread(server, port);
+
+
+    // Connect to server
+    sf::UdpSocket socket;
+    socket.setBlocking(true);
+    sf::Packet packet;
+
+    packet << "hello";
+    socket.send(packet, ip, port);
+
+    socket.setBlocking(false);
 
     const auto MOVE_LEFT = sf::Keyboard::A;
     const auto MOVE_RIGHT = sf::Keyboard::D;
@@ -64,6 +193,8 @@ int main()
     // FPS counter
     sf::Clock fpsClock;
     sf::Clock frameTime;
+    float packetCooldown = 1.f; // seconds
+    float packetTimer = packetCooldown;
     std::size_t frameCounter = 0;
     sf::Font font;
     font.loadFromFile("C:\\Windows\\Fonts\\arial.ttf");
@@ -98,7 +229,7 @@ int main()
                     window.close();
                     break;
                 case FIRE:
-                    bullets.push_back(new Bullet(5, sf::Color::Red, character.m_orientation, character.m_position));
+                    bullets.push_back(new Bullet(5, sf::Color::Red, character.m_orientation, character.m_position + (character.getRadius() + 5.f) * character.m_orientation));
                     break;
                 default:
                     break;
@@ -110,7 +241,7 @@ int main()
         }
 
         sf::Vector2f mouseOrientation(window.mapPixelToCoords(sf::Mouse::getPosition(window)) - character.m_position);
-        float mouseOrientationLength = std::sqrt(mouseOrientation.x * mouseOrientation.x + mouseOrientation.y * mouseOrientation.y);
+        float mouseOrientationLength = norm(mouseOrientation);
         character.m_orientation = sf::Vector2f(mouseOrientation.x / mouseOrientationLength, mouseOrientation.y / mouseOrientationLength);
 
 
@@ -141,18 +272,82 @@ int main()
         ++frameCounter;
 
 
-        // draw the frame
-        window.clear();
-        window.draw(character);
-        window.draw(text);
+        
+        while (true)
+        {
+            sf::IpAddress receivedIp;
+            unsigned short receivedPort;
 
-        for (auto bullet : bullets) {
-            bullet->update(delta);
-            window.draw(*bullet);
+            // receive packet
+            packet.clear();
+
+            sf::Socket::Status status = socket.receive(packet, receivedIp, receivedPort);
+
+            // Done, NotReady, Error, Disconnected
+
+            if (status == sf::Socket::NotReady) {
+                break;
+            } else if (status == sf::Socket::Error) {
+                std::cout << "Client: sf::UdpSocket::Error while receiving packet" << std::endl;
+                break;
+            } else if (status == sf::Socket::Disconnected) {
+                std::cout << "Client: sf::UdpSocket::Disconnected what the fuck even happend" << std::endl;
+                break;
+            }
+
+            // if from our server
+            if (ip == receivedIp && port == receivedPort) {
+                std::cout << "Client: sf::UdpSocket::Done received valid packet" << std::endl;
+                //packet << ;
+            }
         }
 
+        // draw the frame
+        window.clear();
+        
+        bool playerCollided = false;
+        
+        // Bullets
+        for (auto bullet : bullets) {
+            bullet->update(delta);
+            playerCollided = collision(character, *bullet);
+
+            window.draw(*bullet);
+        }
+        if (playerCollided)
+        {
+            character.hit();
+        }
+
+        character.update(delta);
+        // Player
+        window.draw(character);
+
+        // Overlay
+        window.draw(text);
+
+        // Send packet
+        packet.clear();
+
+        
+        packet << static_cast<std::uint16_t>(bullets.size()) << character;
+        for (auto bullet : bullets) {
+            packet << *bullet;
+        }
+        packetTimer -= delta;
+        if (packetTimer<= 0) {
+            sf::Socket::Status status = socket.send(packet, ip, port);
+            if (status == sf::Socket::Done) {
+                std::cout << "Client: sf::UdpSocket::Done successfully sent package" << std::endl;
+            }
+            packetTimer = packetCooldown;
+        }
+        
         window.display();
     }
+
+    std::cout << "Server still running." << std::endl;
+    serverThread.join();
 
     return 0;
 }
